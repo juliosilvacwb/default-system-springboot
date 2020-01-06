2d4e3b70573679d5710fe5f360f18d1a6ee5985c
Julio Silva <julio@adsim.com.br>
2020-01-06 12:44:14 -0300

    Remove swagger 3.0 due it isnot in milestone

--- src/main/java/com/springboot/api/config/SwaggerConfiguration.java
+++ src/main/java/com/springboot/api/config/SwaggerConfiguration.java
@@ -6,21 +6,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+// import springfox.documentation.builders.ApiInfoBuilder;
+// import springfox.documentation.builders.PathSelectors;
+// import springfox.documentation.builders.RequestHandlerSelectors;
+// import springfox.documentation.service.ApiInfo;
+// import springfox.documentation.spi.DocumentationType;
+// import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+// import springfox.documentation.spring.web.plugins.Docket;
+// import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * SwaggerConfiguration
  */
-@Configuration
-@EnableSwagger2WebMvc
-@Import(SpringDataRestConfiguration.class)
+// @Configuration
+// @EnableSwagger2WebMvc
+// @Import(SpringDataRestConfiguration.class)
 public class SwaggerConfiguration {
 
     private static final String PATH_MAPPING = "/";
